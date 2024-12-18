b346b2eeec4887a9efe19361a6fdbb4f7734140c
Endorsiii <leagueofyasuo78@gmail.com>
2024-12-18 21:12:14 +0800

    message

--- src/components/listings/tech.vue
+++ src/components/listings/tech.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="text-center text-[#1b3c59] text-lg font-medium">
+    <div v-if="isEmpty" class="text-center text-[#1b3c59] text-lg font-medium">
         No Tech Listings
     </div>
 </template>
+
+<script lang="ts">
+    const isEmpty = computed(() => myListings.value.length === 0);
+</script>
